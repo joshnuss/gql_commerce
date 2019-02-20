7aad0614d02c01c3f9fee2e26d8714b4e26bebe3
Josh Nussbaum <joshnuss@gmail.com>
2019-02-20 10:39:30 -0500

    Bootstraps gql server

---
+++ server.js
@@ -0,0 +1,16 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    foo: String!
+  }
+`;
+const resolvers = {};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(4001)
+  .then(({url}) => console.log(`Listening on ${url}`));
